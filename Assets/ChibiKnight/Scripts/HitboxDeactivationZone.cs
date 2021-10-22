1300380c667c83aae02c067aa528cacf376d3356
Adrianne Joseph Chua <chua.adrianne@gmail.com>
2021-10-22 18:03:24 +0800

    commit deactivationzone

--- Assets/ChibiKnight/Scripts/HitboxDeactivationZone.cs
+++ Assets/ChibiKnight/Scripts/HitboxDeactivationZone.cs
@@ -7,7 +7,7 @@ public class HitboxDeactivationZone : MonoBehaviour
     private Collider2D HitBox;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponentInParent<HitboxHolder>() != null)
+        if (collision.GetComponentInParent<HitboxHolder>() != null && collision.GetComponent<Collider2D>().gameObject.name != "AggroSensor")
         {
             HitBox = collision.GetComponentInParent<HitboxHolder>().SendingHitBox;
             HitBox.enabled = true;
@@ -15,7 +15,7 @@ public class HitboxDeactivationZone : MonoBehaviour
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.GetComponentInParent<HitboxHolder>() != null)
+        if (collision.GetComponentInParent<HitboxHolder>() != null && collision.GetComponent<Collider2D>().gameObject.name != "AggroSensor")
         {
             HitBox = collision.GetComponentInParent<HitboxHolder>().SendingHitBox;
             HitBox.enabled = true;
@@ -23,7 +23,7 @@ public class HitboxDeactivationZone : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.GetComponentInParent<HitboxHolder>() != null)
+        if (collision.GetComponentInParent<HitboxHolder>() != null && collision.GetComponent<Collider2D>().gameObject.name != "AggroSensor")
         {
             HitBox = collision.GetComponentInParent<HitboxHolder>().SendingHitBox;
             HitBox.enabled = false;
